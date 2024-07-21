df5efa4af7391d56a880cfee43343b1a74a52d4f
liammeyer <76468475+liammeyer@users.noreply.github.com>
2024-07-20 21:47:26 -0500

    Update FLAlgo.py

--- FLAlgo.py
+++ FLAlgo.py
@@ -20,3 +20,55 @@ example_element['label'].numpy()
 plt.imshow(example_element['pixels'].numpy(), cmap='gray', aspect='equal')
 plt.grid(False)
 _ = plt.show()
+
+## Example MNIST digits for one client
+figure = plt.figure(figsize=(20, 4))
+j = 0
+
+for example in example_dataset.take(40):
+  plt.subplot(4, 10, j+1)
+  plt.imshow(example['pixels'].numpy(), cmap='gray', aspect='equal')
+  plt.axis('off')
+  j += 1
+
+
+
+
+
+# Number of examples per layer for a sample of clients
+f = plt.figure(figsize=(12, 7))
+f.suptitle('Label Counts for a Sample of Clients')
+for i in range(6):
+  client_dataset = emnist_train.create_tf_dataset_for_client(
+      emnist_train.client_ids[i])
+  plot_data = collections.defaultdict(list)
+  for example in client_dataset:
+    # Append counts individually per label to make plots
+    # more colorful instead of one color per plot.
+    label = example['label'].numpy()
+    plot_data[label].append(label)
+  plt.subplot(2, 3, i+1)
+  plt.title('Client {}'.format(i))
+  for j in range(10):
+    plt.hist(
+        plot_data[j],
+        density=False,
+        bins=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+
+# Each client has different mean images, meaning each client will be nudging
+# the model in their own directions locally.
+
+for i in range(5):
+  client_dataset = emnist_train.create_tf_dataset_for_client(
+      emnist_train.client_ids[i])
+  plot_data = collections.defaultdict(list)
+  for example in client_dataset:
+    plot_data[example['label'].numpy()].append(example['pixels'].numpy())
+  f = plt.figure(i, figsize=(12, 5))
+  f.suptitle("Client #{}'s Mean Image Per Label".format(i))
+  for j in range(10):
+    mean_img = np.mean(plot_data[j], 0)
+    plt.subplot(2, 5, j+1)
+    plt.imshow(mean_img.reshape((28, 28)))
+    plt.axis('off')
